f5e7895f7da5712f42d6a4fd3fd35a7a634c0df6
Pratap Suryawanshi <pratapsuryawanshi007@gmail.com>
2024-07-12 00:06:14 +0530

    add json url domain

--- DecisionsMaking/vite.config.js
+++ DecisionsMaking/vite.config.js
@@ -1,7 +1,37 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+// import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // base:'/',
+  // resolve: {
+  //   alias: {
+  //     '@components': './src/components',
+  //     '@assets': './src/assets',
+  //     '@styles': './src/styles',
+  //     '@utils': './src/utils',
+  //   },
+  // },
+  // server: {
+  //   port: 3000,
+  //   open: true,
+  // },
+  // css: {
+  //   preprocessorOptions: {
+  //     scss: {
+  //       additionalData: `@import "@styles/variables.scss";`,
+  //     },
+  //   },
+  // },
+  // build: {
+  //   outDir: 'dist',
+  //   minify: true,
+  // },
+  // optimizeDeps: {
+  //   exclude: ['react-dom'],
+  // },
+ 
+
   plugins: [react()],
 })
